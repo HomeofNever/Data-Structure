1d5d0bc5be274268d080dbfc1a25c8152b327ad3
NeverBehave <gayhub@never.pet>
2019-02-20 13:49:30 -0500

    lab6

---
+++ lab/6th/cmake-build-debug/CMakeFiles/6th.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/6th.dir/checkpoint2.cpp.o"
+  "6th.pdb"
+  "6th"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/6th.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
